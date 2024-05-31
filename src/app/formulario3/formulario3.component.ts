5d44617952c7143c58baeaf2adf225cca175d8f4
H-KAFKA <aliagaoq@gmail.com>
2024-05-31 11:00:12 -0500

    Practica 18

--- src/app/formulario3/formulario3.component.ts
+++ src/app/formulario3/formulario3.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-formulario3',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf, JsonPipe],
   templateUrl: './formulario3.component.html',
   styleUrl: './formulario3.component.css'
 })
 export class Formulario3Component {
-  name = new FormControl('Javier');
-  email = new FormControl('javier@info.com');
+
+  get name(){
+    return this.formUser.get('name') as FormControl;
+  }
+
+  get email(){
+    return this.formUser.get('email') as FormControl;
+  }
+
+  formUser = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email])
+  })
+
+
 }
