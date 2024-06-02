907b4bfea93572904d0b7861d7e4b23d754a6a72
h-kafka <aliagaoq@gmail.com>
2024-06-02 14:40:24 -0500

    Practica 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Formulario3Component } from "./formulario3/formulario3.component";
+import { ChildrenComponent } from "./children/children.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, Formulario3Component]
+    imports: [RouterOutlet, Formulario3Component, ChildrenComponent, FormsModule]
 })
 export class AppComponent {
   title = 'CURSO_ANGULAR';
